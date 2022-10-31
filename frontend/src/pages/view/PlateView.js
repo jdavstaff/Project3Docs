63b48a0291dbde725434549c86e75f3c42c5b0e5
Sofia-Ortega <s.ortega@tamu.edu>
2022-10-31 10:48:54 -0500

    ordering - fixed bug in entree selection

--- frontend/src/pages/view/PlateView.js
+++ frontend/src/pages/view/PlateView.js
@@ -3,34 +3,57 @@ import { Button } from "@mui/material";
 import SelectButtons from "../../components/SelectButtons/SelectButtons";
 
 function EntreeSelection({ entreeData, handleEntreeSelect }) {
-  const [myEntree, setMyEntree] = useState([...entreeData]);
-
-  const handleSelect = (id) => {
-    const updatedData = myEntree.map((item) => {
-      item.selected = item.id === id;
-      return item;
-    });
-    setMyEntree(updatedData);
-  };
-
   return (
     <div>
       <h4>Entrees</h4>
       <div>
-        <SelectButtons items={myEntree} handleSelect={handleSelect} />
+        <SelectButtons items={entreeData} handleSelect={handleEntreeSelect} />
       </div>
     </div>
   );
 }
+
+const dummyData = [
+  {
+    name: "orange chicken",
+    selected: false,
+    id: 1,
+  },
+  {
+    name: "orange beef",
+    selected: false,
+    id: 2,
+  },
+  {
+    name: "green rice",
+    selected: true,
+    id: 3,
+  },
+  {
+    name: "walnut beef",
+    selected: false,
+    id: 651453,
+  },
+  {
+    name: "brocolli shrimp",
+    selected: false,
+    id: 89543,
+  },
+];
+
+const dummyData2 = dummyData;
 
 // user will either be "cashier" or "client"
 export default function PlateView({ user, handleView }) {
   // NOTE: Data MUST have keys: id : int, selected : bool, and name : string
   const [sideData, setSideData] = useState([]);
   const [entreeData, setEntreeData] = useState([]);
+  const [entreeData2, setEntreeData2] = useState([]);
+  const [entreeData3, setEntreeData3] = useState([]);
 
   useEffect(() => {
     // FIXME: AXIOS call to get entree and side data
+    // [ {string name, int id,}]
     // to update sideData and entreeData, use the syntax:
     // setSideData ( **NEW DATA** )
     // setEntreeData ( **NEW DATA ** )
@@ -48,37 +71,26 @@ export default function PlateView({ user, handleView }) {
       },
       {
         name: "purple rice",
-        selected: true,
+        selected: false,
         id: 3,
       },
     ]);
 
-    setEntreeData([
-      {
-        name: "orange chicken",
-        selected: false,
-        id: 1,
-      },
-      {
-        name: "orange beef",
-        selected: false,
-        id: 2,
-      },
-      {
-        name: "green rice",
-        selected: true,
-        id: 3,
-      },
-      {
-        name: "walnut beef",
-        selected: false,
-        id: 651453,
-      },
-      {
-        name: "brocolli shrimp",
-        selected: false,
-        id: 89543,
-      },
+    let myData = dummyData;
+
+    setEntreeData([...dummyData]);
+    setEntreeData2([
+      { name: "sofia", id: 444, selected: false },
+      { name: "josh", id: 21, selected: true },
+      { name: "mitchell", id: 21321, selected: false },
+      { name: "sebastian", id: 12321, selected: false },
+    ]);
+    setEntreeData3([
+      { name: "frank", id: 123454, selected: false },
+      { name: "frank", id: 12354, selected: false },
+      { name: "frank", id: 12454, selected: false },
+      { name: "frank", id: 12344, selected: false },
+      { name: "frank", id: 12345, selected: false },
     ]);
   }, []);
 
@@ -91,11 +103,29 @@ export default function PlateView({ user, handleView }) {
   };
 
   const handleEntreeSelect = (id) => {
+    console.log("111");
     const updatedData = entreeData.map((item) => {
       item.selected = item.id === id;
       return item;
     });
     setEntreeData(updatedData);
+  };
+  const handleEntreeSelect2 = (id) => {
+    console.log("222");
+    const updatedData = entreeData2.map((item) => {
+      item.selected = item.id === id;
+      return item;
+    });
+    setEntreeData2(updatedData);
+  };
+
+  const handleEntreeSelect3 = (id) => {
+    console.log("333");
+    const updatedData = entreeData3.map((item) => {
+      item.selected = item.id === id;
+      return item;
+    });
+    setEntreeData3(updatedData);
   };
 
   const handleAddBtn = () => {
@@ -117,12 +147,12 @@ export default function PlateView({ user, handleView }) {
         handleEntreeSelect={handleEntreeSelect}
       />
       <EntreeSelection
-        entreeData={entreeData}
-        handleEntreeSelect={handleEntreeSelect}
+        entreeData={entreeData2}
+        handleEntreeSelect={handleEntreeSelect2}
       />
       <EntreeSelection
-        entreeData={entreeData}
-        handleEntreeSelect={handleEntreeSelect}
+        entreeData={entreeData3}
+        handleEntreeSelect={handleEntreeSelect3}
       />
       <Button
         variant="outlined"
