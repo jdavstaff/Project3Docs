cf61234a8f534005edf32bc3e2b921e58865f78e
sebastian-reyes50 <prof.reyes.sebastian50@gmail.com>
2022-11-07 10:49:36 -0600

    maybe

--- frontend/src/pages/customer/Customer.js
+++ frontend/src/pages/customer/Customer.js
@@ -2,12 +2,14 @@ import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import Header from "../../components/Header";
 import OrderView from "../view/OrderView";
+import MapComponent from "../../components/Map/mapComponent";
 
 export default function Customer() {
   return (
     <div>
       <Header name={"customer"} />;
-      <OrderView user={"customer"} />
+      <OrderView user={"customer"} />;
+      <MapComponent/>;
     </div>
   );
 }
