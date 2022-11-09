81875d0bec9c6f8d3a653b017a74b48af91512e3
Sofia Ortega <s.ortega@tamu.edu>
2022-11-08 18:19:17 -0600

    fixed bug on map component

--- frontend/src/components/Map/mapComponent.js
+++ frontend/src/components/Map/mapComponent.js
@@ -1,21 +1,20 @@
-import React, { Component, useMemo } from "react";
 import { useLoadScript, GoogleMap } from "@react-google-maps/api";
 
-export function Mapper(){
-	const {isLoaded} = useLoadScript({
-		googleMapsApiKey: process.env.NEXT_PUBLIC_KEY,
-	})
+export default function Mapper() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_KEY,
+  });
 
-	if (!isLoaded) return <div>Loading...</div>;
-	return <Map />;
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map />;
 }
 
-function Map(){
-	return <GoogleMap 
-					zoom={10}
-					center = {{lat:10, lng: -40}}
-					mapContainerClassName="class_map" // use for sass...
-					>
-					
-					</GoogleMap>
+function Map() {
+  return (
+    <GoogleMap
+      zoom={10}
+      center={{ lat: 10, lng: -40 }}
+      mapContainerClassName="class_map" // use for sass...
+    ></GoogleMap>
+  );
 }
