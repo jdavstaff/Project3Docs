4f38b5da9d9833b472ffbc90c372b2058eac6530
sebastian-reyes50 <prof.reyes.sebastian50@gmail.com>
2022-12-05 11:57:12 -0600

    Added map logic, added map input by user

--- frontend/src/components/Map/MapComponent.js
+++ frontend/src/components/Map/MapComponent.js
@@ -1,34 +1,74 @@
 import "./Map.css"
-import { useLoadScript, GoogleMap } from "@react-google-maps/api";
-import { useMemo } from "react";
+import { useLoadScript, GoogleMap, StandaloneSearchBox, Marker } from "@react-google-maps/api";
+import { useMemo, useState } from "react";
 
+const lib = ['places'];
+const containerStyle = {
+  width: '100%',
+  height: '50vh'
+}
 export default function Mapper() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_KEY,
+    libraries: lib
   });
 
   if (!isLoaded) return <div>Loading...</div>;
   return <Map />;
 }
 
-const containerStyle = {
-  width: '100%',
-  height: '50vh'
-}
+
 
 function Map() {
+  const center = useMemo(() => ({lat: 30.622370, lng: -96.325851}), []);
+  const [searchBox, setSearchBox] = useState(null);
+  const [selected, setSelected] = useState(center);
 
-  const center = useMemo(() => ({lat: 10, lng: -40}), []);
+  const onPlacesChanged = () => {
+    //searchBox.getPlaces();
+    //console.log(searchBox.getPlaces()[0]);
+    setSelected(searchBox.getPlaces()[0].geometry.location.toJSON());
+  };
+  
+  const onLoad = ref => { setSearchBox(ref); };
+  
 
   return (
+    <>
     <div className="Wrapper">
       <div className="MapContainer">
         <GoogleMap
         zoom={8}
-        center={center}
+        center={selected}
         mapContainerStyle={containerStyle}>
+            <StandaloneSearchBox
+            onPlacesChanged={onPlacesChanged}
+            onLoad={onLoad}>
+            <input
+              type="text"
+              placeholder="Customized your placeholder"
+              style={{
+                boxSizing: 'border-box',
+                border: `1px solid transparent`,
+                width: `270px`,
+                height: `40px`,
+                padding: `0 12px`,
+                borderRadius: `3px`,
+                boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                fontSize: `14px`,
+                outline: `none`,
+                margin: 'center',
+                textOverflow: `ellipses`,
+                position: 'absolute',
+                top: '40px',
+                marginLeft: '50%'
+              }}
+            />
+            </StandaloneSearchBox>
+            selected && <Marker position={selected}/>
         </GoogleMap>
       </div>
-    </div>  
+    </div>
+    </>  
   );
 }
